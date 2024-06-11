a450ce7e5aea01c836a8fbd5bd01dc696b649378
Jarol Molina <jarolmolina@gmail.com>
2024-06-11 16:34:20 -0500

    MD Analisis Gromacs to .npy

--- mdanalysis.py
+++ mdanalysis.py
@@ -1,7 +1,7 @@
 import numpy as np
 import MDAnalysis as mda
 from utilities import conv_inputs
-
+PDB = $"exports/home/jarol.molina/20dp_dna/DNA_1.pdb"
 u = mda.Universe("/exports/home/jarol.molina/DNA-1/MD.gro", "/exports/home/jarol.molina/DNA-1/MD.trr")
 dna=u.select_atoms("nucleic")
 
@@ -24,7 +24,7 @@ with open('data/DNA1_Fxyz.npy', 'wb') as g:
 
 
 # Creacion de array de nombres                                           
-an = conv_inputs('exports/home/jarol.molina/20dp_dna/DNA_1.pdb')
+an = conv_inputs($PDB)
 with open('data//DNA1_Z.npy', 'wb') as f:
    np.save(f,an.title())
 print(len(an.title()))
